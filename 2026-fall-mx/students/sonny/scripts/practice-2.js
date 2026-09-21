1e6c316280fbf16c128d70a73b5d5881c1d0a39d
Sonny <sonnyhsieh.5in@gmail.com>
2026-09-21 14:51:27 +0800

    function

--- 2026-fall-mx/students/sonny/scripts/practice-2.js
+++ 2026-fall-mx/students/sonny/scripts/practice-2.js
@@ -1,2 +1,18 @@
 const button = document.getElementById("my-button");
 console.log(button);
+
+const title = document.getElementById("title");
+console.log(title);
+
+function testMybutton(event){
+    console.log("Listen to my button!", event);
+}
+
+testMybutton("NOW");
+
+button.addEventListener("click", testMybutton);
+
+function testMyBody(event){
+    console.log("Listen to my body!", event);
+}
+document.body.addEventListener("click", testMyBody);
